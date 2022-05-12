edf3010e9e13aea17fb91321b20f97b510295fdb
Nika Qvrivishvili <nika.qvrivishviliPC@gmail.com>
2022-05-12 23:18:52 +0400

    core logic works

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,39 @@
     windows_subsystem = "windows"
 )]
 
+use std::process::{Command, Stdio};
+use tauri::{Manager, Window};
+use std::io::{BufReader, BufRead};
+
+async fn listener(window: Window){
+    let mut cmd = Command::new("lib/listener")
+        .stdout(Stdio::piped())
+        .spawn()
+        .unwrap();
+
+    {
+        let stdout = cmd.stdout.as_mut().unwrap();
+        let stdout_reader = BufReader::new(stdout);
+        let stdout_lines = stdout_reader.lines();
+
+        for line in stdout_lines {
+            window.emit("download", line.as_ref().unwrap());
+            println!("{}", line.unwrap());
+        }
+    }
+
+    cmd.wait().unwrap();
+}
+
 fn main() {
     tauri::Builder::default()
+    .setup(|app| {
+        let main_window = app.get_window("main").unwrap();
+        tauri::async_runtime::spawn(
+            listener(main_window)
+        );
+        Ok(())
+      })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -1,32 +1,44 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Styles from './styles.module.css'
 import { Command } from '@tauri-apps/api/shell'
-declare global {
-    interface Window {
-        __TAURI__: any
-    }
-}
-const Form: React.FC = () => {
-    const [URL, setURL] = useState('')
+import { listen } from '@tauri-apps/api/event'
 
-    const DownloadMusic = async (event: React.FormEvent<HTMLButtonElement>) => {
-        const command = new Command('yt-download', [
-            'arg0',
-            'arg1',
-            'D:\\Test\\%(title)s.%(ext)s',
-            URL,
-        ])
-        console.log((await command.execute()).stdout)
-        console.log(await command.execute())
+const Form: React.FC = () => {
+    const [URL, setURL] = useState('https://www.youtube.com/watch?v=tH94YuQtg-8')
+    const [title, setTitle] = useState('None')
+    const [progress, setProgress] = useState(0)
+
+    const DownloadMusic = async () => {
+        const command = Command.sidecar('lib/test', [URL])
+        command.stdout.on('data', async (line) => {
+            const json = await JSON.parse(line)
+            setTitle(json.title)
+            setProgress(json.progress)
+        })
     }
 
     const UpdateURL = (event: React.ChangeEvent<HTMLInputElement>) => {
         setURL(event.currentTarget.value)
     }
 
+    useEffect(() => {
+        listen('download', (event: { payload: string }) => {
+            setURL(event.payload)
+            const command = Command.sidecar('lib/test', [event.payload])
+            command.stdout.on('data', async (line) => {
+                const json = await JSON.parse(line)
+                setTitle(json.title)
+                setProgress(json.progress)
+            })
+            command.spawn()
+        })
+    }, [])
+
     return (
         <form className={Styles['form']}>
             <h2 className={Styles['title']}>Download music by YT URL</h2>
+            <h3 className={Styles['title']}>{title}</h3>
+            <div>{progress}</div>
             <input
                 className={Styles['url-input']}
                 type={'text'}
